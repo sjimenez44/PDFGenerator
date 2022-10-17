a230bae1d76c4fa59ed1af46fab9759c87e05b40
Santiago Jimenez <info@bufferorigin.com>
2022-10-17 15:59:19 -0500

    Add Template document

---
+++ PDFGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using PDFGenerator.DeliveryReceipt;
+using PDFGenerator.OrderConfirmation;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace PDFGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Orden de confirmacion");
+            OrderConfirmationModel data = new OrderConfirmationModel()
+            {
+                OrderNumber = 1234,
+                IssueDate = DateTime.Now,
+                OrderInformation = new OrderInformation(),
+                Items = new List<OrderItem>(),
+                Comments = "Recuerda que puedes hacerle seguimiento al pedido ingresando el numero de la orden en la pagina web: http://www.bufferorigin.com/seguimientopedidos/"
+            };
+            OrderConfirmationDocument order = new OrderConfirmationDocument(data);
+            order.GeneratePdf("order.pdf");
+
+            Console.WriteLine("Recibo de entrega");
+            DeliveryReceiptModel dataDelivery = new DeliveryReceiptModel()
+            {
+                IssueDate = DateTime.Now
+            };
+            DeliveryReceiptDocument delivery = new DeliveryReceiptDocument(dataDelivery);
+            delivery.GeneratePdf("delivery.pdf");
+        }
+    }
+}
---
+++ PDFGenerator/TemplateDocument.cs
@@ -0,0 +1,39 @@
+﻿using PDFGenerator.DeliveryReceipt;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace PDFGenerator
+{
+    public abstract class TemplateDocument<T> : IDocument
+    {
+        public T Model { get; }
+
+        public TemplateDocument(T model)
+        {
+            Model = model;
+        }
+
+        public DocumentMetadata GetMetadata()
+        {
+            DocumentMetadata metadata = DocumentMetadata.Default;
+            metadata.Producer = "BufferOrigin";
+            return metadata;
+        }
+
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+                page.Margin(40);
+                page.Header().Element(ComposeHeader);
+                page.Content().Element(ComposeContent);
+                page.Footer().Element(ComposeFooter);
+            });
+        }
+
+        public abstract void ComposeHeader(IContainer container);
+        public abstract void ComposeContent(IContainer container);
+        public abstract void ComposeFooter(IContainer container);
+    }
+}
